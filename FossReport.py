337313931a434a7ad40c3072b0ea588178d969f2
Alex_Ber <45004572+alextestgit@users.noreply.github.com>
2022-12-11 20:23:44 +0200

    clean code before create new report
    clean sources before create new report

--- FossReport.py
+++ FossReport.py
@@ -3,63 +3,23 @@ import csv
 
 import fossOutlook
 import os
-import glob
-# import subprocess
 import extract_msg
-import datetime
-import win32com.client
-
 import xlwings as xw
-
-# from xls2xlsx import XLS2XLSX
 
 INPUT_FOLDER = "\\sources\\"
 OUTPUT_FOLDER = "output"
 SECTIONS = {"Critical vulnerabilities": "Critical", "High vulnerabilities": "High"}
-# critical = []
-# high = []
+
 new_report = []
 xlsx_files = []
 csv_files = []
 csv_reader = None
 
 
-def remove_changes(value):
-    for char in "[]'":
-        value = value.replace(char, '')
-    return value
-
-
 def openExcel(full_filename):
     global ws
     ws = xw.Book(full_filename).sheets[0]
 
-
-def get_section_row(start_row, name):
-    for row in range(start_row, 1000):
-        cell = f'A{row}'
-        value = str(ws.range(cell).value)
-        if name in value:
-            print(f"---------- find {name} in row {row} ---------")
-            break
-    return row + 3
-
-
-def pint_data(row, name):
-    global critical, high
-    for row_ in range(row, 100):
-        cell = f'A{row_}:F{row_}'
-        row_value = str(ws.range(cell).value)
-        row_value = row_value.split(",")
-        row0 = remove_changes(row_value[0])
-        if "None" not in row0:
-            print(row0)
-            if "Critical" in name:
-                critical.append(row_value)
-            else:
-                high.append(row_value)
-        else:
-            break
 
 
 def work_with_csv(filename):
@@ -90,33 +50,6 @@ def get_section_data(product, severity):
         report_row.insert(len(row), severity)
         new_report.append(report_row)
 
-    # def work_with_csv(filename):
-    open_csv_file(filename)
-    for section_name in SECTIONS:
-        move2section_date(section_name)
-        get_section_data()
-
-
-def work_with_excel(file):
-    openExcel(file)
-    section_row = 1
-    for section_name in SECTIONS:
-        section_row = get_section_row(section_row, section_name)
-        pint_data(section_row, section_name)
-
-
-#
-# def convertxls2xlsx(in_filename, out_filename):
-#     x2x = XLS2XLSX(in_filename)
-#     x2x.to_xlsx(out_filename)
-
-# def open_message(file_):
-#     # outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-#     # file_path = SOURCE_FOLDER + file_
-#     # msg = outlook.OpenSharedItem(file_path)
-#     msg = extract_msg.Message(SOURCE_FOLDER + file_)
-#     # https://stackoverflow.com/questions/26322255/parsing-outlook-msg-files-with-python
-#     return msg
 
 
 def extract_excels_from_msg():
@@ -142,17 +75,6 @@ def is_folder_empty(path: str) -> bool:
     return len(os.listdir(path)) == 0
 
 
-# def convert_xls2xlsx():
-#     # https://stackoverflow.com/questions/1858195/convert-xls-to-csv-on-command-line
-#     global xlsx_files
-#     xls_files = glob.glob(SOURCE_FOLDER + "*.xls")
-#     xls_files = [xls_file for xls_file in xls_files if "~" not in xls_file]
-#     for xls_file in xls_files:
-#         xlsx_file = os.path.splitext(xls_file)[0] + '.xlsx'
-#         cmd = f'{SOURCE_FOLDER}XlsToCsv1.vbs {xls_file} {xlsx_file}'
-#         returned_output = os.system(cmd)
-#         print(f'Converter output: {returned_output}')
-#         xlsx_files.append(xls_file)
 
 def convert_xls2csv():
     cmd = f'{directory_path}\XlsToCsv.vbs {SOURCE_FOLDER}'
--- fossOutlook.py
+++ fossOutlook.py
@@ -1,48 +1,51 @@
- # Function to log in to Microsoft Outlook
+
+# Function to log in to Microsoft Outlook
 def login(username, password):
-  # Create a Microsoft Outlook object
-  outlook = win32com.client.Dispatch("Outlook.Application")
+    # Create a Microsoft Outlook object
+    outlook = win32com.client.Dispatch("Outlook.Application")
 
-  # Log in to your Microsoft Outlook account
-  outlook.Session.Logon(username, password)
+    # Log in to your Microsoft Outlook account
+    outlook.Session.Logon(username, password)
 
-  return outlook
+    return outlook
+
 
 # Function to read emails from a specified folder
 def read_emails(outlook, folder_name):
-  # Get the specified folder from Microsoft Outlook
-  folder = outlook.Session.Folders[folder_name]
+    # Get the specified folder from Microsoft Outlook
+    folder = outlook.Session.Folders[folder_name]
 
-  # Iterate through the emails in the folder
-  for email in folder.Items:
-    # Return the email object
-    yield email
+    # Iterate through the emails in the folder
+    for email in folder.Items:
+        # Return the email object
+        yield email
+
 
 # Function to save an email and its attachments to a specified directory
 def save_email(email, save_dir):
-  # Save the email to the specified directory
-  email.SaveAs(save_dir + "/" + email.Subject + ".msg")
+    # Save the email to the specified directory
+    email.SaveAs(save_dir + "/" + email.Subject + ".msg")
 
-  # Iterate through the attachments in the email
-  for attachment in email.Attachments:
-    # Save the attachment to the specified directory
-    attachment.SaveAsFile(save_dir + "/" + attachment.Filename)
+    # Iterate through the attachments in the email
+    for attachment in email.Attachments:
+        # Save the attachment to the specified directory
+        attachment.SaveAsFile(save_dir + "/" + attachment.Filename)
+
 
 # Main program
 if __name__ == "__main__":
-  # Log in to Microsoft Outlook
-  username = "your_username"
-  password = "your_password"
-  outlook = login(username, password)
+    # Log in to Microsoft Outlook
+    username = "your_username"
+    password = "your_password"
+    outlook = login(username, password)
 
-  # Read emails from the Inbox folder
-  folder_name = "Inbox"
-  for email in read_emails(outlook, folder_name):
-    # Save the email and its attachments to the specified directory
-    save_dir = "C:/email_backup"
-    save_email(email, save_dir)
+    # Read emails from the Inbox folder
+    folder_name = "Inbox"
+    for email in read_emails(outlook, folder_name):
+        # Save the email and its attachments to the specified directory
+        save_dir = "C:/email_backup"
+        save_email(email, save_dir)
 
-  # Close the Microsoft Outlook object
-  outlook.Quit()
+    # Close the Microsoft Outlook object
+    outlook.Quit()
 
-# ----------   Outlook end --------------------------------
